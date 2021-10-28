60d08518b3bbec04cf9716440c611be16360f05b
vsuhaas02@gmail.com <vsuhaas02@gmail.com>
2021-10-28 10:22:29 +0530

    day - 3

---
+++ 30daysofcoding/day3.cpp
@@ -0,0 +1,45 @@
+//Day - 3, Topic: Arrays;
+//Question link: https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1;
+//Sort an array of 0s, 1s and 2s
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i = 0;i < n;i++)
+    {
+        cin>>a[i];
+    }
+    int c0=0,c1=0,c2=0;
+    for(int i = 0;i < n;i++)
+    {
+        if(a[i]==0)
+        {
+            c0++;
+        }
+        else if(a[i]==1)
+        {
+            c1++;
+        }
+        else
+        {
+            c2++;
+        }
+    }
+    for(int i = 0;i < c0;i++)
+    {
+        cout<<0<<" ";
+    }
+    for(int i = c0;i < c0+c1;i++)
+    {
+        cout<<1<<" ";
+    }
+    for(int i = c0+c1;i < n;i++)
+    {
+        cout<<2<<" ";
+    }
+    return 0;
+}
+//time complexity - O(n)
