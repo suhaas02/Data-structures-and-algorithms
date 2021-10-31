40a66d3e2b1a0c4186d267c1ce83d7ffca41b255
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2021-10-31 16:08:15 +0530

    day - 6

--- 30daysofcoding/day6.cpp
+++ 30daysofcoding/day6.cpp
@@ -47,3 +47,37 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+// //Day - 6 Topic: Arrays
+// //Minimize the height;
+// //Problem link: https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n,k;
+//     cin>>n>>k;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         if ((arr[i] - k) <= 0)
+//         {
+//             arr[i] = arr[i] + k;
+//         }
+//         else
+//         {
+//             arr[i]  = arr[i] - k;
+//         }
+//     }
+//     sort(arr, arr + n);
+//     int height = arr[n - 1] - arr[0];
+//     cout<<height<<endl;
+//     return 0;
+// }
