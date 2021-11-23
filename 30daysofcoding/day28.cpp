1cf9b7a96769c517e467b92ba6ac89097a321caa
suhaas02 <vsuhaas02@gmail.com>
2021-11-23 23:22:25 +0530

    day - 28

--- 30daysofcoding/day28.cpp
+++ 30daysofcoding/day28.cpp
@@ -20,9 +20,35 @@
 //     return 0;
 // }
 
-//Day -28 //starters 18 #codechef
-//problem link: https://www.codechef.com/START18C/problems/MAKEEQUAL
-//Make them equal
+// //Day -28 //starters 18 #codechef
+// //problem link: https://www.codechef.com/START18C/problems/MAKEEQUAL
+// //Make them equal
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;
+//     cin>>t;
+//     while (t--){
+//         int n;
+//         cin>>n;
+//         vector<int> a(n);
+//         for (int i = 0; i < n; i++){
+//             cin>>a[i];
+//         }
+//         int maxx = *max_element(a.begin(), a.end());
+//         int ans = 0;
+//         for(auto x : a){
+//             ans = max(ans,maxx - x);
+//         }
+//         cout<<ans<<endl;
+//     }
+//     return 0;
+// }
+
+//day - 28; #codechef
+//problem link: https://www.codechef.com/START18C/problems/STRCOMPARE
+//Compare those strings
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -32,16 +58,18 @@ int main()
     while (t--){
         int n;
         cin>>n;
-        vector<int> a(n);
+        string a,b;
+        cin>>a;
+        cin>>b;
+        int count = 0;
         for (int i = 0; i < n; i++){
-            cin>>a[i];
+            string s1 = a.substr(i, n - i);
+            string s2 = b.substr(i, n - i);
+            if (s1 < s2){
+                count++;
+            }  
         }
-        int maxx = *max_element(a.begin(), a.end());
-        int ans = 0;
-        for(auto x : a){
-            ans = max(ans,maxx - x);
-        }
-        cout<<ans<<endl;
+        cout<<count<<endl;
     }
     return 0;
 }
