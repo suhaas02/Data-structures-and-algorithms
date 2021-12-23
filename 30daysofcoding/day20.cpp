35c45ba69805c26513fba5e4cfecdd66f250ed7d
suhaas02 <vsuhaas02@gmail.com>
2021-12-23 22:56:28 +0530

    modified

--- 30daysofcoding/day20.cpp
+++ 30daysofcoding/day20.cpp
@@ -6,25 +6,20 @@ using namespace std;
 int median(int [], int);
 int getmedian(int arr1[],int arr2[],int n)
 {
-    if (n <= 0)
-    {
+    if (n <= 0){
         return -1;
     }
-    if (n == 1)
-    {
+    if (n == 1) {
         return (arr1[0] + arr2[0])/2;
     }
-    if (n == 2)
-    {
+    if (n == 2){
         return (max(arr1[0], arr2[0]) + min(arr1[1], arr2[1]))/2;
     }
     int m1 = median(arr1, n);
     int m2 = median(arr2, n);
     if (m1 == m2)
         return m1;
- 
-    if (m1 < m2)
-    {
+    if (m1 < m2){
         if (n % 2 == 0)
             return getmedian(arr1 + n / 2 - 1, arr2, n - n / 2 + 1);
         return getmedian(arr1 + n / 2, arr2, n - n / 2);
@@ -32,7 +27,6 @@ int getmedian(int arr1[],int arr2[],int n)
     if (n % 2 == 0)
         return getmedian(arr2 + n / 2 - 1, arr1, n - n / 2 + 1);
     return getmedian(arr2 + n / 2, arr1, n - n / 2);
-
 }
 
 int median(int arr[], int n)
@@ -47,21 +41,13 @@ int main()
     int n1,n2;
     cin>>n1>>n2;
     int arr1[n1], arr2[n2];
-    for (int i = 0; i < n1; i++)
-    {
+    for (int i = 0; i < n1; i++){
         cin>>arr1[n1];
     }
-    for (int j = 0; j < n2; j++)
-    {
+    for (int j = 0; j < n2; j++){
         cin>>arr2[n2];
     }
-    if (n1 == n2)
-    {
+    if (n1 == n2){
         cout<<"Median: "<<getmedian(arr1,arr2,n1)<<endl;
     }
-    else
-    {
-        cout << "Doesn't work for arrays "<<endl;      
-    }
-    
 }
--- 30daysofcoding/day24.cpp
+++ 30daysofcoding/day24.cpp
@@ -1,35 +1,89 @@
-//Day-24 topic: arrays
-//problem link: https://www.geeksforgeeks.org/find-subarray-with-given-sum/
-//Find subarray with given sum
+// //Day-24 topic: arrays
+// //problem link: https://www.geeksforgeeks.org/find-subarray-with-given-sum/
+// //Find subarray with given sum
+// #include<bits/stdc++.h>
+// using namespace std;
+// int findSubarray(int arr[], int n, int sum)
+// {
+//     int current_sum = 0, i, j;
+//     for (int i = 0; i < n; i++){
+//         current_sum = arr[i];
+//         for ( j = i + 1; j <= n; j++){
+//             if (current_sum == sum){
+//                 cout<<"Sum found between indexes "<<i<<" and "<<j - 1<<endl;
+//                 return 1;
+//             }
+//             if (current_sum > sum || j == n){
+//                 break;
+//             }
+//             current_sum = current_sum + arr[j];
+//         }
+//     }
+//     cout<<"No subrray found"<<endl;
+//     return 0;
+// }
+// int main()
+// {
+//     int n,sum;
+//     cin>>n>>sum;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     findSubarray(arr,n,sum);
+// }
+
+//Day - 24, topic: arrays
+//problem link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+//Best time to buy and sell stock
 #include<bits/stdc++.h>
 using namespace std;
-int findSubarray(int arr[], int n, int sum)
-{
-    int current_sum = 0, i, j;
-    for (int i = 0; i < n; i++){
-        current_sum = arr[i];
-        for ( j = i + 1; j <= n; j++){
-            if (current_sum == sum){
-                cout<<"Sum found between indexes "<<i<<" and "<<j - 1<<endl;
-                return 1;
-            }
-            if (current_sum > sum || j == n){
-                break;
-            }
-            current_sum = current_sum + arr[j];
-        }
-    }
-    cout<<"No subrray found"<<endl;
-    return 0;
-}
 int main()
 {
-    int n,sum;
-    cin>>n>>sum;
+    int n,profit;
+    cin>>n;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin>>arr[i];
     }
-    findSubarray(arr,n,sum);
+    int min = 0;
+    int max = 0;
+    for (int j = 1; j < n; j++)
+    {
+        if (arr[j] > max)
+        {
+            max = j;
+        }
+        else if(arr[j] < min)
+        {
+            min = j;
+        }
+    }
+    if (max < min)
+    {
+        cout<<"Not feasible to buy stocks"<<endl;
+    }
+    else
+    {
+        cout<<arr[max] - arr[min]<<endl;
+    }
+    return 0;
 }
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int profit = 0;
+        for (int i = prices.size() - 1; i >= 0; i--) {
+            if (prices[i] > max) {
+                max = prices[i];
+            } else {
+                profit = std::max(profit, max - prices[i]);
+            }
+        }
+        return profit;
+    }
+};
