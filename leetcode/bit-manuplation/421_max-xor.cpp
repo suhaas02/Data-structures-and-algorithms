2770c0c4de26832417a405464704f513f6ab84f9
suhaas02 <vsuhaas02@gmail.com>
2022-01-28 17:27:18 +0530

    searching &sorting

---
+++ leetcode/bit-manuplation/421_max-xor.cpp
@@ -0,0 +1,8 @@
+//maximum xor of two numbers in an array;
+//problem link: https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/description/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+}
