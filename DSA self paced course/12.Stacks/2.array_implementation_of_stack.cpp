ee53852325ed75dcd9291d7014a1ddc2a64466e9
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2022-06-25 19:51:50 +0530

    stacks

---
+++ DSA self paced course/12.Stacks/2.array_implementation_of_stack.cpp
@@ -0,0 +1,94 @@
+//implementation of stack using array
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// struct myStack
+// {
+//     int *arr;
+//     int cap;
+//     int top;
+//     myStack(int c)
+//     {
+//         cap = c;
+//         arr = new int[cap];
+//         top = -1;
+//     }
+//     void push(int x)
+//     {
+//         top++;
+//         arr[top] = x;
+//     }
+//     int pop()
+//     {
+//         int res = arr[top];
+//         top--;
+//         return res;
+//     }
+//     int peek()
+//     {
+//         return arr[top];
+//     }
+//     int size()
+//     {
+//         return (top + 1);
+//     }
+//     bool isEmpty()
+//     {
+//         return (top == -1);
+//     }
+// };
+
+// int main()
+// {
+//     myStack s(5);
+//     s.push(10);
+//     s.push(20);
+//     s.push(30);
+//     cout<<s.pop()<<endl;
+//     cout<<s.size()<<endl;
+//     cout<<s.peek()<<endl;
+//     cout<<s.isEmpty()<<endl;
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+struct myStack
+{
+    vector<int> v;
+    void push(int x)
+    {
+        v.push_back(x);
+    }
+    int pop()
+    {
+        int res = v.back();
+        v.pop_back();
+        return res;
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool isEmpty()
+    {
+        return v.empty();
+    }
+    int peek()
+    {
+        return v.back();
+    }
+};
+
+int main()
+{
+    myStack v ;
+    v.push(10);
+    v.push(20);
+    v.push(30);
+    cout<<v.pop()<<endl;
+    cout<<v.size()<<endl;
+    cout<<v.peek()<<endl;
+    cout<<v.isEmpty()<<endl;
+    return 0;
+}
