dabc2c51b645d7abe1a58d26c3d69285026d5318
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2022-07-30 22:49:02 +0530

    bst

--- DSA self paced course/16.Binary_search_tree/25.find_kth_smallest_bst.cpp
+++ DSA self paced course/16.Binary_search_tree/25.find_kth_smallest_bst.cpp
@@ -1,55 +1,55 @@
-// //finding kth smallest element in bst 
+//finding kth smallest element in bst 
 
-// //naive approach
-// //tc: o(n)
-// //space: o(1), augmented bst
-// #include<bits/stdc++.h>
-// using namespace std;
+//naive approach
+//tc: o(n)
+//space: o(1), augmented bst
+#include<bits/stdc++.h>
+using namespace std;
 
-// struct node
-// {
-//     int key;
-//     node *left;
-//     node *right;
-//     node(int k)
-//     {
-//         key = k;
-//         left = right = NULL;
-//     }
-// };
+struct node
+{
+    int key;
+    node *left;
+    node *right;
+    node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
 
-// void printKth(node *root,int k,int count)
-// {
-//     if (root != NULL)
-//     {
-//         printKth(root -> left,k,count);
-//         count++;
-//         if (count == k)
-//         {
-//             cout<<root -> key<<endl;
-//             return;
-//         }
-//         printKth(root -> right,k,count);
+void printKth(node *root,int k,int count)
+{
+    if (root != NULL)
+    {
+        printKth(root -> left,k,count);
+        count++;
+        if (count == k)
+        {
+            cout<<root -> key<<endl;
+            return;
+        }
+        printKth(root -> right,k,count);
         
-//     }
+    }
     
-// }
+}
 
-// int main() {
+int main() {
 	
-// 	node *root=new node(15);
-// 	root->left=new node(5);
-// 	root->left->left=new node(3);
-// 	root->right=new node(20);
-// 	root->right->left=new node(18);
-// 	root->right->left->left=new node(16);
-// 	root->right->right=new node(80);
-// 	int k=3;
-// 	int count=0;
-// 	cout<<"Kth Smallest: ";
-// 	printKth(root,k,count);
+	node *root=new node(15);
+	root->left=new node(5);
+	root->left->left=new node(3);
+	root->right=new node(20);
+	root->right->left=new node(18);
+	root->right->left->left=new node(16);
+	root->right->right=new node(80);
+	int k=3;
+	int count=0;
+	cout<<"Kth Smallest: ";
+	printKth(root,k,count);
 	
-// 	return 0;
-// }
+	return 0;
+}
 
  
