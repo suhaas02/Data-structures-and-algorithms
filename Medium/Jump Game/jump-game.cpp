6568bf8708931698095472232b96edc06e16f1f6
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-06-24 13:37:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+        int jump = 0;
+        for(int i = 0; i < N; i++)
+        {
+            if(jump < i)
+            {
+                return 0;
+            }
+            jump = max(jump, i + A[i]);
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
