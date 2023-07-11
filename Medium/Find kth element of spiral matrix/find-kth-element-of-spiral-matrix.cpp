8758b1a540084c9f32b9cf60fc0f700eafb83d5e
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-07-11 21:49:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find kth element of spiral matrix/find-kth-element-of-spiral-matrix.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    /*You are required to complete this method*/
+    int findK(int a[MAX][MAX],int n,int m,int k)
+    {
+ 		// Your code goes here.
+ 		vector<int> ans;
+ 		int top = 0, left = 0, bottom = n - 1, right = m - 1;
+ 		while(top <= bottom and left <= right)
+ 		{
+ 		    for(int i = left; i <= right; i++)
+ 		    {
+ 		        ans.push_back(a[top][i]);
+ 		    }
+ 		    top++;
+ 		    for(int i = top; i <= bottom; i++)
+ 		    {
+ 		        ans.push_back(a[i][right]);
+ 		    }
+ 		    right--;
+ 		    for(int i = right; i >= left; i--)
+ 		    {
+ 		        ans.push_back(a[bottom][i]);
+ 		    }
+ 		    bottom--;
+ 		    for(int i = bottom; i >= top; i--)
+ 		    {
+ 		        ans.push_back(a[i][left]);
+ 		    }
+ 		    left++;
+ 		}
+ 		return ans[k - 1];
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+  
+    while(T--)
+    {
+        int n,m;
+        int k=0;
+        //cin>>k;
+        cin>>n>>m>>k;
+        int a[MAX][MAX];
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.findK(a,n,m,k)<<endl;
+        
+       
+    }
+}
+// } Driver Code Ends
