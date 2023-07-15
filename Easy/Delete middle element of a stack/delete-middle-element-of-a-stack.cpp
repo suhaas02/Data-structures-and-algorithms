235e65f8c49bb20889fcc5d108fdc7b4aa387765
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-07-15 19:00:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete middle element of a stack/delete-middle-element-of-a-stack.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        // code here.. 
+        int mid = ceil((sizeOfStack+1)/2);
+
+        stack<int> st;
+
+        for(int i=0;i<sizeOfStack;i++){
+
+            if(i != sizeOfStack-mid){
+
+               st.push(s.top());
+
+            }
+
+             s.pop();
+
+        }
+
+        
+
+        for(int i=0;i<sizeOfStack-1;i++){
+
+            int temp = st.top();
+
+            st.pop();
+
+            s.push(temp);
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
