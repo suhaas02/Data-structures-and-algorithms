7c841d509bc2d012d47189188f7e9ba6938aec4a
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-07-16 21:16:53 +0530

    Time: 96 ms (31.25%) | Memory: 24.5 MB (46.70%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
-    int helper(int l1, int l2, string &text1, string &text2, vector<vector<int>> &dp)
-    {
-        if(l1 < 0 or l2 < 0)
-            return 0;
-        if(dp[l1][l2] != -1)
-            return dp[l1][l2];
-        if(text1[l1] == text2[l2])
-        {
-            return dp[l1][l2] = 1 + helper(l1 - 1, l2 - 1, text1, text2, dp);
-        }
-        return dp[l1][l2] = max(helper(l1 - 1, l2, text1, text2, dp), helper(l1, l2 - 1, text1, text2, dp));
-    }
+    // int helper(int l1, int l2, string &text1, string &text2, vector<vector<int>> &dp)
+    // {
+    //     if(l1 < 0 or l2 < 0)
+    //         return 0;
+    //     if(dp[l1][l2] != -1)
+    //         return dp[l1][l2];
+    //     if(text1[l1] == text2[l2])
+    //     {
+    //         return dp[l1][l2] = 1 + helper(l1 - 1, l2 - 1, text1, text2, dp);
+    //     }
+    //     return dp[l1][l2] = max(helper(l1 - 1, l2, text1, text2, dp), helper(l1, l2 - 1, text1, text2, dp));
+    // }
     int longestCommonSubsequence(string text1, string text2) {
+        // int l1 = text1.size();
+        // int l2 = text2.size();
+        // vector<vector<int>> dp(l1, vector<int> (l2, -1));
+        // return helper(l1 - 1, l2 - 1, text1, text2, dp);
+
         int l1 = text1.size();
         int l2 = text2.size();
-        vector<vector<int>> dp(l1, vector<int> (l2, -1));
-        return helper(l1 - 1, l2 - 1, text1, text2, dp);
+        vector<vector<int>> dp(l1 + 1, vector<int> (l2 + 1, -1));
+        //here we need to shift the index by 1 place, because we cant keep the array index as -1;'
+        for(int i = 0; i <= l1; i++)
+        {
+            dp[i][0] = 0;
+        }
+        for(int i = 0; i <= l2; i++)
+        {
+            dp[0][i] = 0;
+        }
+
+        for(int i = 1; i <= l1; i++)
+        {
+            for(int j = 1; j <= l2; j++)
+            {
+                if(text1[i - 1] == text2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[l1][l2];
     }
 };
