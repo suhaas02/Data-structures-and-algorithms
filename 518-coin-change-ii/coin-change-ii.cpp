dc8c846ff6df72038c391ba4d8e111e0b17b603c
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-07-16 13:46:59 +0530

    Time: 66 ms (18.06%) | Memory: 29.6 MB (7.10%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,55 @@
 class Solution {
 public:
-    long long helper(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp)
-    {
-        if(ind == 0)
-        {
-            if(amount % coins[ind] == 0)
-                return 1;
-            else
-                return 0;
-        }
-        if(dp[ind][amount] != -1)
-            return dp[ind][amount];
-        long long notPick = helper(ind - 1, amount, coins, dp);
-        long long pick = 0;
-        if(coins[ind] <= amount)
-        {
-            pick =  helper(ind, amount - coins[ind], coins, dp);
-        }
-        return dp[ind][amount] = pick + notPick;
-    }
+    // long long helper(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp)
+    // {
+    //     if(ind == 0)
+    //     {
+    //         if(amount % coins[ind] == 0)
+    //             return 1;
+    //         else
+    //             return 0;
+    //     }
+    //     if(dp[ind][amount] != -1)
+    //         return dp[ind][amount];
+    //     long long notPick = helper(ind - 1, amount, coins, dp);
+    //     long long pick = 0;
+    //     if(coins[ind] <= amount)
+    //     {
+    //         pick =  helper(ind, amount - coins[ind], coins, dp);
+    //     }
+    //     return dp[ind][amount] = pick + notPick;
+    // }
     int change(int amount, vector<int>& coins) {
+        // int n = coins.size();
+        // vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+        // int ans = helper(n - 1, amount, coins, dp);
+        // return ans;
+
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
-        int ans = helper(n - 1, amount, coins, dp);
-        return ans;
+        vector<vector<long>> dp(n, vector<long> (amount + 1, 0));
+        
+        //base case
+        for(int i = 0; i <= amount; i++)
+        {
+            if(i % coins[0] == 0)
+            {
+                dp[0][i] = 1;
+            }
+        }
+
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j <= amount; j++)
+            {
+                long long notPick = dp[i - 1][j];
+                long long pick = 0;
+                if(coins[i] <= j)
+                {
+                    pick =  dp[i][j - coins[i]];
+                }
+                dp[i][j] = pick + notPick;
+            }
+        }
+        return dp[n - 1][amount];
     }
 };
