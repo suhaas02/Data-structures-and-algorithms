935de4637029be57700feb02ea2c6031bedc5607
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-07-21 21:28:19 +0530

    Time: 25 ms (50.59%) | Memory: 14.5 MB (28.94%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,57 @@
 class Solution {
 public:
-    int helper(vector<int> &prices, int ind, int n, int buy, int tran, vector<vector<vector<int>>> &dp)
-    {
-        if(ind == n)
-            return 0;
-        if(tran ==0)
-            return 0;
-        if(dp[ind][buy][tran] != -1)
-            return dp[ind][buy][tran];
-        if(buy == 1)
-        {
-            return dp[ind][buy][tran] = max(-prices[ind] + helper(prices, ind + 1, n, 0, tran, dp), helper(prices, ind + 1, n, 1, tran, dp));
-        }
-        return dp[ind][buy][tran] = max(prices[ind] + helper(prices, ind + 1, n, 1, tran - 1, dp), helper(prices, ind + 1, n, 0, tran, dp));
-    }
+    // int helper(vector<int> &prices, int ind, int n, int buy, int tran, vector<vector<vector<int>>> &dp)
+    // {
+    //     if(ind == n)
+    //         return 0;
+    //     if(tran ==0)
+    //         return 0;
+    //     if(dp[ind][buy][tran] != -1)
+    //         return dp[ind][buy][tran];
+    //     if(buy == 1)
+    //     {
+    //         return dp[ind][buy][tran] = max(-prices[ind] + helper(prices, ind + 1, n, 0, tran, dp), helper(prices, ind + 1, n, 1, tran, dp));
+    //     }
+    //     return dp[ind][buy][tran] = max(prices[ind] + helper(prices, ind + 1, n, 1, tran - 1, dp), helper(prices, ind + 1, n, 0, tran, dp));
+    // }
     int maxProfit(int k, vector<int>& prices) {
+        // int n = prices.size();
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        // return helper(prices, 0, n, 1, k, dp);
+
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
-        return helper(prices, 0, n, 1, k, dp);
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+        for(int i = 0; i <= 1; i++)
+        {
+            for(int j = 0; j <= k; j++)
+            {
+                dp[n][i][j] = 0;
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j <= 1; j++)
+            {
+                dp[i][j][0] = 0;
+            }
+        }
+
+        //now copy the recurrence
+        for(int ind = n - 1; ind >= 0; ind--)
+        {
+            for(int buy = 0; buy <= 1; buy++)
+            {
+                for(int tran = 1; tran <= k; tran++)
+                {
+                    if(buy == 1)
+                    {
+                        dp[ind][buy][tran] = max(-prices[ind] + dp[ind + 1][0][tran], dp[ind + 1][1][tran]);
+                    }
+                    else
+                        dp[ind][buy][tran] = max(prices[ind] + dp[ind + 1][1][tran - 1], dp[ind + 1][0][tran]);
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
