22e3f3b0fde737c956d1287562b21b4a42c36b76
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-07-24 10:16:11 +0530

    Time: 50 ms (62.29%) | Memory: 18.5 MB (67.25%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -20,25 +20,48 @@ public:
         // return helper(s,t,ans, 0, n);
     // }
 
-    int helper(string s, string t, int n, int m, vector<vector<int>> &dp)
-    {
-        if(m < 0)
-            return 1;
-        if(n < 0)
-            return 0;
-        if(dp[n][m] != -1)
-            return dp[n][m];
-        if(s[n] == t[m])
-        {
-            return dp[n][m] = helper(s, t, n - 1, m  - 1, dp) + helper(s, t, n - 1, m, dp);
-        }
-        else
-            return dp[n][m] = helper(s, t, n - 1, m, dp);
-    }
+    //memoization approach
+    // int helper(string s, string t, int n, int m, vector<vector<int>> &dp)
+    // {
+    //     if(m < 0)
+    //         return 1;
+    //     if(n < 0)
+    //         return 0;
+    //     if(dp[n][m] != -1)
+    //         return dp[n][m];
+    //     if(s[n] == t[m])
+    //     {
+    //         return dp[n][m] = helper(s, t, n - 1, m  - 1, dp) + helper(s, t, n - 1, m, dp);
+    //     }
+    //     else
+    //         return dp[n][m] = helper(s, t, n - 1, m, dp);
+    // }
     int numDistinct(string s, string t)
     {
+        // int n = s.length(), m = t.length();
+        // vector<vector<int>>dp(n, vector<int> (m, -1));
+        // return helper(s, t,n - 1, m - 1, dp);
+
+        //tabulation approach 
+        int mod = 1e9+7;
+
         int n = s.length(), m = t.length();
-        vector<vector<int>>dp(n, vector<int> (m, -1));
-        return helper(s, t,n - 1, m - 1, dp);
+        vector<vector<int>>dp(n + 1, vector<int> (m + 1, 0));
+        for(int i=0;i<= n;i++){
+            dp[i][0]=1;
+        }
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(s[i - 1] == t[j - 1])
+                {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                }
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][m];
     }
 };
