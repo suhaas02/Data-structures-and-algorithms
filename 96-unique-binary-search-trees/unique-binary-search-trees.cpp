f3e63c68795e9521eceb16a7500604689cdac881
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-08-16 11:00:23 +0530

    Time: 2 ms (55.12%) | Memory: 6.1 MB (41.59%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> ans(n + 1, 0);
+        ans[1] = 1;
+        ans[0] = 1;
+        for(int i = 2; i <= n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                ans[i] = ans[i] + (ans[j] * ans[i - j - 1]);
+            }
+        }
+        return ans[n];
+    }
+};
