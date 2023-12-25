77b954cd6b1205c9e0a738d454ac9c9f1f6d8afa
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-12-25 11:48:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove duplicate elements from sorted Array/remove-duplicate-elements-from-sorted-array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int A[],int N){
+        // code here
+         int result=1;
+        for(int i=1;i<N;i++){
+            if(A[result-1]!=A[i]){
+                A[result]=A[i];
+                result++;
+            }
+        }
+        return result;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}
+// } Driver Code Ends
