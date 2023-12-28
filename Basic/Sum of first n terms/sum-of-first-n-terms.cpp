7423dcb00537290e2234efcaed4b38f1acb42bc1
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-12-28 11:43:08 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Sum of first n terms/sum-of-first-n-terms.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long sumOfSeries(long long N) {
+        // code here
+        if(N == 1 or N == 2)
+            return N;
+        long long int sum = 1;
+        for(int i = 2; i <= N; i++)
+        {
+            sum += powl(i, 3);
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfSeries(N) << "\n";
+    }
+}
+// } Driver Code Ends
