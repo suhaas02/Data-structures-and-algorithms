a0b9723cbe784ea335f02e1e3375a2e44805070a
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-12-29 17:23:35 +0530

    Updated Solution - GfG to GitHub

--- Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -7,15 +7,15 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    int mod = 1e9 + 7;
     int nthFibonacci(int n){
         // code here
-        vector<int> dp(n + 2);
+        int mod=1000000007;
+        vector<int> dp(n + 1, 0);
         dp[0] = 0;
         dp[1] = 1;
-        for(int i = 2; i < n + 1; i++)
+        for(int i = 2; i <= n; i++ )
         {
-            dp[i] = (dp[i - 1] % mod + dp[i - 2] % mod) % mod;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
         }
         return dp[n] % mod;
     }
