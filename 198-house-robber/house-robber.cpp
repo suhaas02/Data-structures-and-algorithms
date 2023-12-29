b3eafb2120f005dc4dbd71b3ca00a97a4bc9aace
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2023-12-29 11:26:10 +0530

    Time: 8 ms (24.06%) | Memory: 8.2 MB (18.89%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int helper(vector<int> &nums, int ind, vector<int> &dp)
+    {
+        if(ind == 0)
+            return nums[ind];
+        if(ind < 0)
+            return 0;
+        if(dp[ind] != -1)
+            return dp[ind];
+        int pick = nums[ind] + helper(nums, ind - 2, dp);
+        int notPick =  helper(nums, ind - 1, dp);
+        return dp[ind] = max(pick, notPick);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return helper(nums, n - 1, dp);
+    }
+};
