8a96edf2a5edc20f1b55ed75bb83713376724809
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2024-01-03 16:39:19 +0530

    Time: 91 ms (12.61%) | Memory: 51 MB (16.49%) - LeetSync

--- 3202-high-access-employees/high-access-employees.cpp
+++ 3202-high-access-employees/high-access-employees.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
-        unordered_map<string, vector<int>> mp;
-        int n = access_times.size();
-        for(int i = 0; i < n; i++)
+        unordered_map<string, vector<string>> mp;
+        for(int i = 0; i < access_times.size(); i++)
         {
-            mp[access_times[i][0]].push_back(stoi(access_times[i][1]));
-        }
-        vector<string> ans;
-        for(auto x : mp)
-        {
-            vector<int> temp = x.second;
-            sort(temp.begin(), temp.end());
-            for(int i = 2; i < temp.size(); i++)
+            if(mp.find(access_times[i][0]) != mp.end())
             {
-                if(temp[i] - temp[i - 2] < 100)
+                mp[access_times[i][0]].push_back(access_times[i][1]);
+            }
+            else
+            {
+                mp[access_times[i][0]].push_back(access_times[i][1]);
+            }
+        }
+        for(auto x: mp)
+        {
+            cout << x.first << " ";
+            for(auto y : x.second)
+            {
+                cout << y << " ";
+            }
+            cout << endl;
+        }
+
+        vector<string> ans;
+        for(auto entry : mp)
+        {
+            if(entry.second.size() > 2)
+            {
+                vector<string> temp = entry.second;
+                sort(temp.begin(), temp.end());
+                for(int i = 2; i < temp.size(); i++)
                 {
-                    ans.push_back(x.first);
-                    break;
+                    int x = stoi(temp[i]);
+                    int y = stoi(temp[i - 2]);
+                    if(x - y < 100)
+                    {
+                        ans.push_back(entry.first);
+                        break;
+                    }
                 }
             }
         }
