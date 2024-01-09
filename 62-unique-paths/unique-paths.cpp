adfd0c01c2736ff157fbc0349b5d0c9ce0d925bd
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2024-01-09 14:46:54 +0530

    Time: 4 ms (10.45%) | Memory: 7 MB (20.59%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    int helper(int m, int n, vector<vector<int>> &dp)
-    {
-        if(m < 0 or n < 0)
-            return 0;
-        if(m == 0 and n == 0)
-            return 1;
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        int left = helper(m, n - 1, dp);
-        int up = helper(m - 1, n, dp);
-        return dp[m][n] = (left + up);
-    }
+
+    //memoization approach
+    // int helper(int m, int n, vector<vector<int>> &dp)
+    // {
+    //     if(m < 0 or n < 0)
+    //         return 0;
+    //     if(m == 0 and n == 0)
+    //         return 1;
+    //     if(dp[m][n] != -1)
+    //         return dp[m][n];
+    //     int left = helper(m, n - 1, dp);
+    //     int up = helper(m - 1, n, dp);
+    //     return dp[m][n] = (left + up);
+    // }
     int uniquePaths(int m, int n) {
         // int count = 0;
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return helper(m - 1, n - 1, dp);
+        // vector<vector<int>> dp(m, vector<int> (n, -1));
+        // return helper(m - 1, n - 1, dp);
+
+        //tabulation appraoch
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i == 0 and j == 0)
+                {
+                    dp[0][0] = 1;
+                }
+                else
+                {
+                    int right = 0, left = 0;
+                    if(i > 0)   
+                        right = dp[i - 1][j];
+                    if(j > 0)
+                        left = dp[i][j - 1];
+                    dp[i][j] = left + right;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
