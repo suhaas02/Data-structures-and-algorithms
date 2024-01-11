305d7ce07712b2baf414e8bc7c77dd437fdf0dc7
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2024-01-11 14:27:46 +0530

    Time: 9 ms (77.37%) | Memory: 9 MB (42.75%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    static bool cmp(pair<int,int> &a, pair<int,int> &b)
-    {
-        return a.first > b.first;
-    }
+    // static bool cmp(pair<int,int> &a, pair<int,int> &b)
+    // {
+    //     return a.first > b.first;
+    // }
     string frequencySort(string s) {
-        unordered_map<int,int> mp;
+        // unordered_map<int,int> mp;
+        // for(auto x : s)
+        //     mp[x]++;
+        // string ans;
+        // vector<pair<int,int>> vec;
+        // for(auto x : mp)
+        // {
+        //     vec.push_back({x.second, x.first});
+        // }
+        // sort(vec.begin(), vec.end(), cmp);
+        // for(auto x : vec)
+        // {
+        //     while(x.first--)
+        //         ans += x.second;
+        // }
+        // return ans;
+
+        //approach - 2
+        //using priority queue
+        unordered_map<char,int> mp;
         for(auto x : s)
             mp[x]++;
-        string ans;
-        vector<pair<int,int>> vec;
+        priority_queue<pair<int,char>> pq;
         for(auto x : mp)
         {
-            vec.push_back({x.second, x.first});
+            pq.push({x.second, x.first});
         }
-        sort(vec.begin(), vec.end(), cmp);
-        for(auto x : vec)
+        string ans;
+        while(!pq.empty())
         {
-            while(x.first--)
-                ans += x.second;
+            auto it = pq.top();
+            for(int i = 0; i < it.first; i++)
+            {
+                ans += it.second;
+            }
+            pq.pop();
         }
         return ans;
     }
