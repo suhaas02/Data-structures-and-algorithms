90909ab3be46c847a09fb5aca1171fa830958e2a
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2024-01-21 15:24:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    int helper(vector<int> &nums, int ind, vector<int> &dp)
-    {
-        if(ind == 0)
-            return nums[ind];
-        if(ind < 0)
-            return 0;
-        if(dp[ind] != -1)
-            return dp[ind];
-        int pick = nums[ind] + helper(nums, ind - 2, dp);
-        int notPick =  helper(nums, ind - 1, dp);
-        return dp[ind] = max(pick, notPick);
-    }
+    // int helper(vector<int> &nums, int ind, vector<int> &dp)
+    // {
+    //     if(ind == 0)
+    //         return nums[ind];
+    //     if(ind < 0)
+    //         return 0;
+    //     if(dp[ind] != -1)
+    //         return dp[ind];
+    //     int pick = nums[ind] + helper(nums, ind - 2, dp);
+    //     int notPick =  helper(nums, ind - 1, dp);
+    //     return dp[ind] = max(pick, notPick);
+    // }
     int rob(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<int> dp(n, -1);
+        // return helper(nums, n - 1, dp);
+
+        //trying tabulation approach
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return helper(nums, n - 1, dp);
+        vector<int> dp(n, 0); 
+        dp[0] = nums[0];
+        for(int i = 1; i < n; i++)
+        {
+            int pick = nums[i];
+            if(i > 1)
+                pick += dp[i - 2];
+            int notPick = dp[i - 1];
+            dp[i] = max(pick, notPick);
+        }
+        return dp[n - 1];
     }
 };
