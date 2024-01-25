b1e1c7fcc650a7be4fba5d55d296a6e943017b1d
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2024-01-25 18:45:41 +0530

    Time: 32 ms (78.36%) | Memory: 25.7 MB (6.35%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // int helper(int l1, int l2, string &text1, string &text2, vector<vector<int>> &dp)
+    // int helper(string text1, string text2, int l1, int l2, vector<vector<int>> &dp)
     // {
     //     if(l1 < 0 or l2 < 0)
     //         return 0;
@@ -8,29 +8,19 @@ public:
     //         return dp[l1][l2];
     //     if(text1[l1] == text2[l2])
     //     {
-    //         return dp[l1][l2] = 1 + helper(l1 - 1, l2 - 1, text1, text2, dp);
+    //         return dp[l1][l2] = 1 + helper(text1, text2, l1 - 1, l2 - 1, dp);
     //     }
-    //     return dp[l1][l2] = max(helper(l1 - 1, l2, text1, text2, dp), helper(l1, l2 - 1, text1, text2, dp));
+    //     return dp[l1][l2] = max(helper(text1, text2, l1 - 1, l2, dp), helper(text1, text2, l1, l2 - 1, dp));
     // }
     int longestCommonSubsequence(string text1, string text2) {
-        // int l1 = text1.size();
-        // int l2 = text2.size();
+        // int l1 = text1.length(), l2 = text2.length();
         // vector<vector<int>> dp(l1, vector<int> (l2, -1));
-        // return helper(l1 - 1, l2 - 1, text1, text2, dp);
+        // return helper(text1, text2, l1 - 1, l2 - 1, dp);
+        
 
-        int l1 = text1.size();
-        int l2 = text2.size();
-        vector<vector<int>> dp(l1 + 1, vector<int> (l2 + 1, -1));
-        //here we need to shift the index by 1 place, because we cant keep the array index as -1;'
-        for(int i = 0; i <= l1; i++)
-        {
-            dp[i][0] = 0;
-        }
-        for(int i = 0; i <= l2; i++)
-        {
-            dp[0][i] = 0;
-        }
-
+        //tabulation approach 
+        int l1 = text1.size(), l2 = text2.size();
+        vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1, 0));
         for(int i = 1; i <= l1; i++)
         {
             for(int j = 1; j <= l2; j++)
@@ -40,7 +30,9 @@ public:
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else
+                {
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
         return dp[l1][l2];
