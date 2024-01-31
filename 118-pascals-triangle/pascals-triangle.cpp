a7ba8099ce1de2cb34770cfb5d2ce3c70e36aa0a
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2024-01-31 14:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (14.02%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -2,40 +2,26 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        vector<int> res;
         if(numRows == 1)
+            return {{1}};
+        ans.push_back({1});
+        if(numRows == 2)
+            return {{1},{1,1}};
+        ans.push_back({1, 1});
+        vector<int> temp;
+        for(int i = 3; i <= numRows; i++)
         {
-            res = {1};
-            ans.push_back(res);
-        }
-        else if(numRows == 2)
-        {
-            res = {1};
-            ans.push_back(res);
-            res = {1, 1};
-            ans.push_back(res);
-        }
-        else
-        {
-            res = {1};
-            ans.push_back(res);
-            res = {1, 1};
-            ans.push_back(res);
-            for(int i = 3; i <= numRows;i++)
+            temp = {};
+            for(int j = 0; j < i; j++)
             {
-                res = {};
-                for(int j = 0; j < i;j++)
+                if(j == 0 or j == i - 1)
+                    temp.push_back(1);
+                else
                 {
-                    if(j == 0 || j == i - 1)
-                        res.push_back(1);
-                    else
-                    {
-                        res.push_back(ans[i - 2][j - 1] + ans[i - 2][j ]);
-                    }
-                    
+                    temp.push_back(ans[i - 2][j - 1] + ans[i - 2][j]);
                 }
-                ans.push_back(res);
             }
+            ans.push_back(temp);
         }
         return ans;
     }
