61f1975343c9698680a2774beca7253a32588324
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2024-02-04 15:43:49 +0530

    Time: 3 ms (72.77%) | Memory: 11.8 MB (24.70%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size(), m = matrix[0].size();
-        int row = 0;
+        int n = matrix.size(); 
+        int m = matrix[0].size();
+        int x = 0;
+        // if(m == 1 and n == 1 and target == matrix[0][0])
+        //     return true;
+        // else
+        //     return false;
         for(int i = 0; i < n; i++)
         {
             if(matrix[i][0] <= target and matrix[i][m - 1] >= target)
             {
-                row = i;
+                x = i;
                 break;
             }
         }
-
         int l = 0, h = m - 1;
         while(l <= h)
         {
             int mid = l + (h - l) / 2;
-            if(matrix[row][mid] == target)
+            if(matrix[x][mid] == target)
                 return true;
-            else if(matrix[row][mid] < target)
+            else if(matrix[x][mid] < target)
+            {   
                 l = mid + 1;
+            }
             else
                 h = mid - 1;
         }
